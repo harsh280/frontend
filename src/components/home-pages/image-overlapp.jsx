5c4cedd0cb3dd09e81b63ad9d0b7d32aa7fbbc40
Harsh <harsh.619.cr7@.com>
2023-10-08 22:05:08 +0530

    fourth commit

--- src/components/home-pages/image-overlapp.jsx
+++ src/components/home-pages/image-overlapp.jsx
@@ -18,7 +18,7 @@ const ImageOverlapp = () => {
             <div className="rounded-xl absolute pl-48 pt-4">
                     <img className = "w-[350px] h-[500px]" src = "https://s.pinimg.com/webapp/center-2d76a691.png"/>
             </div>
-            <div className="w-[350px] h-[100px] bg-white rounded-full absolute lg:left-[150px] left-[20px] top-[200px] flex">
+            <div className="w-[350px] h-[100px] bg-white rounded-full absolute lg:left-[150px] left-[5px] top-[200px] flex">
                 <svg className = "text-center mt-[35px] ml-10" height="24" width="24" viewBox="0 0 24 24" aria-label="search" role="img"><path d="M10 16c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6m13.12 2.88-4.26-4.26A9.842 9.842 0 0 0 20 10c0-5.52-4.48-10-10-10S0 4.48 0 10s4.48 10 10 10c1.67 0 3.24-.41 4.62-1.14l4.26 4.26a3 3 0 0 0 4.24 0 3 3 0 0 0 0-4.24"></path></svg>
                 <h2 className="text-[#6E0F3C] font-bold text-2xl text-center mt-[30px] ml-2">easy chicken dinner</h2>
             </div>
